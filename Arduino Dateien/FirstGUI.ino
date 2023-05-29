3b185e476cea231f7bef2e73057d3cf662737e39
Kevin Klug <67338735+Derion23@users.noreply.github.com>
2023-05-29 18:06:40 +0200

    Added Classes and SD-Card

--- Arduino Dateien/FirstGUI.ino
+++ Arduino Dateien/FirstGUI.ino
@@ -1,7 +1,127 @@
 #include <LCDWIKI_GUI.h> //Core graphics library
 #include <LCDWIKI_KBV.h> //Hardware-specific library
+#include <SD.h>   // libraries for SD card
+#include <SPI.h>
 
 LCDWIKI_KBV mylcd(ILI9486,A3,A2,A1,A0,A4); //model,cs,cd,wr,rd,reset
+
+// CLASSES VARIABLES AND FUNCTIONS
+const int numOfTanks = 6;
+void print(String message);
+
+// CLASSES
+class Ingredient{
+  private:
+    String name;
+    int amount;
+  public:
+    Ingredient(){
+      this->name = "";
+      this->amount = 0;
+    }
+    Ingredient(String name, int amount){
+      this->name = name;
+      this->amount = amount;
+    }
+    void setName(String name){ this->name = name; }
+    void setAmount(int amount){ this->amount = amount; }
+    String getName(){ return this->name; }
+    int getAmount(){ return this->amount; }
+};
+
+class Cocktail{
+  private:
+    String name;
+    Ingredient ingredients[numOfTanks];
+
+  public:
+    Cocktail(){this->name = "";}
+
+    Cocktail(String name, Ingredient ingredients[numOfTanks]){
+      this->name = name;
+      for (int i = 0; i < numOfTanks; i++){
+        this->ingredients[i] = Ingredient(ingredients[i].getName(), ingredients[i].getAmount());
+        if(ingredients[i].getName() == "") break;
+      }
+    }
+
+    String getName() {return this->name;}
+    void setName(String name){this->name = name;}
+    Ingredient getIngredient(int index){return this-> ingredients[index];}
+
+    /*
+    void printCocktail(){
+      print (this->name+":");
+      for (int i = 0; i < numOfTanks; i++){
+      print(" ->" + String(this->ingredients[i].getAmount())+"cl of " + String(this->ingredients[i].getName()));
+      }
+    }
+    */
+
+    void printToConsole(){
+      Serial.print(this->name);
+      for (int i = 0; i < numOfTanks; i++){
+        if(this->ingredients[i].getName() == "") break;
+        Serial.print(" -> " + String(this->ingredients[i].getAmount())+"cl von " + String(this->ingredients[i].getName()));
+      }
+      Serial.println("");
+    }
+};
+
+class Tank{
+  private:
+    int fillingLevel = 0;
+    String content;
+  public:
+    Tank(){
+      this->content = ""; 
+      this->fillingLevel = 0;
+    }
+    void setContent(String content){ this->content = content; }
+    void fillTo(int amount){this->fillingLevel = amount;}
+    void withdraw(int amount){this->fillingLevel -= amount;}
+    String getContent(){  return this->content; }
+    int getAmount(){ return this->fillingLevel;}
+};
+
+class ColtShot{
+  private:
+    Tank tanks[numOfTanks];
+  public:
+    ColtShot(){}
+
+    void addContent(String content){
+      for(int i = 0; i < numOfTanks; i++){
+        if(this->tanks[i].getContent() == "")
+        this->tanks[i].setContent(content);
+        return;
+      }
+    }
+
+    void emptyTanks(){
+      for (int i = 0; i < numOfTanks; i++){
+        this->tanks[i].fillTo(0);
+        this->tanks[i].setContent("");
+      }
+    }
+    
+    void printTanks(){
+      print("Tanks:");
+      for (int i = 0; i < numOfTanks; i++){
+        print("Content: " + this->tanks[i].getContent() + ", Amount: " + char(this->tanks[i].getAmount()) + "cl");
+      }
+    }
+    /*
+    void TestPrintTanks(){
+      print("Tanks:");
+      for (int i = 0; i < numOfTanks; i++){
+        Serial.println("Content: " + this->tanks[i].getContent() + ", Amount: " + String(this->tanks[i].getAmount()) + "cl");
+      }
+    }
+    */
+
+    String getTankContent(int index){ return this->tanks[index].getContent();}
+  };
 
 // CONSTANTS
 const int textPaddingX = 10;
@@ -14,6 +134,10 @@ const int correctionCirclePosY = 3;
 const int choiceMaxPosY = 280;
 const int maxNumOfPages = 5;
 const int textChoiceSpacingY = 15;
+const int maxNumOfCocktails = 20;
+const int maxNumOfIngredients = 40;
+const int separatorLineX = 280;
+const int separatorLineY = 80;
 
 const String supportedIngredients[] = {"Sprudelwasser", "Whiskey", "Limettensaft", "Tequila", "Orangensaft", "Gin", "Vodka", "Rum", "Cola", "TonicWater", "OrangenLikoer", "Sirup", "Martini", "Cachaca", "GrenadineSirup",
 "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"};
@@ -39,7 +163,17 @@ int numOfChoicesOnPage[maxNumOfPages - 1];
 int skippedChoices = 0; // numOfChoices which were not printed because the were on a previous page
 bool previousPageChoicesSkipped = false;
 
-// DREHKNOPF
+ColtShot cs;
+Cocktail cocktails[maxNumOfCocktails];
+String ingredients[maxNumOfIngredients];
+bool chosenIngredients[maxNumOfIngredients];
+
+// SD CARD, name of opened txt file could no be longer than 8 chars
+int pinSD = 53;
+int pinSDVcc = 22;
+int pinSDGnd = 48;
+
+// TURN KNOB
 int pinDT = 20;
 int pinCLK = 21;
 int pinOkBTN = 19;
@@ -72,12 +206,20 @@ void setup() {
   mylcd.Init_LCD();
   mylcd.Set_Rotation(HORIZONTAL);
 
-  // setup Drehknopf
+  // setup Turn knob
   pinMode(pinDT, INPUT_PULLUP);
   pinMode(pinCLK, INPUT_PULLUP);
   pinMode(pinOkBTN, INPUT_PULLUP);
   previousClkState = digitalRead(pinCLK);
-  // INTERRUPTS, possiblw Interrupt PINS: 2, 3, 18, 19, 20, 21
+
+  // setupSD Card
+  pinMode(pinSD, OUTPUT);
+  pinMode(pinSDVcc, OUTPUT);
+  pinMode(pinSDGnd, OUTPUT);
+  digitalWrite(pinSDVcc, HIGH);
+  digitalWrite(pinSDGnd, LOW);
+
+  // INTERRUPTS, possible Interrupt PINS: 2, 3, 18, 19, 20, 21
   attachInterrupt(digitalPinToInterrupt(pinOkBTN), okButtonPress, FALLING); // if pinOkBTN HIGH -> LOW => Button pressed
   attachInterrupt(digitalPinToInterrupt(pinBackBTN), backButtonPress, FALLING); // if back btn was pressed
   attachInterrupt(digitalPinToInterrupt(pinCLK), knobTurn, CHANGE); // if clk Pin Changes
@@ -88,6 +230,17 @@ void setup() {
   mylcd.Set_Text_Back_colour(BLACK);
   mylcd.Set_Text_colour(WHITE);
   mylcd.Set_Text_Size(2);
+
+  // setup ColtShot
+  cs = ColtShot();
+  initChosenIngredientsArr();
+
+  // SD card
+  if(!initializeSDCard()) return;
+
+  downloadCocktailsFromSD();
+  saveIngredientsFromCocktailsToSD();
+  downloadIngredientsFromSD();
 
   Serial.println("Program started...");
 }
@@ -102,8 +255,8 @@ void loop() {
     addIngredients();
   else if(state == "confirmGoingBack")
     confirmGoingBack();
-  else if(state == "end")
-    end();
+  else if(state == "error")
+    error();
 }
 
 
@@ -231,10 +384,9 @@ void confirmGoingBack(){
   }
 }
 
-
-void end(){
+void error(){
   mylcd.Fill_Screen(BLACK);
-  print("End");
+  print("ERROR");
   while(true){
 
   }
@@ -287,9 +439,13 @@ void printAddCocktailsMenu(){
     printChoice("Zutaten hinzufuegen");
   }
 
-  for(int i = 0; i < 8; i++){
-    printChoiceToPages(supportedCocktails[i]);
+  for(int i = 0; i < maxNumOfCocktails; i++){
+    if(cocktails[i].getName() == "") break;
+    printChoiceToPages(cocktails[i].getName());
   }
+
+  // print tanks and separator lines
+  printTanks();
 
   if(currentPage == 0)
     numOfChoices++; // additional "Zutaten hinzufügen" option
@@ -305,14 +461,24 @@ void printAddIngredientsMenu(){
   }
 
   for(int i = 0; i < 25; i++){
-    printChoiceToPages(supportedIngredients[i]);
+    if(ingredients[i] == "") break;
+    printChoiceToPages(ingredients[i]);
   }
+
+  // print tanks and separator lines
+  printTanks();
 
   if(currentPage == 0)
     numOfChoices++; // additional "Cocktails hinzufuegen" option
 }
 
 // HELPER FUCTIONS
+void initChosenIngredientsArr(){
+  for(int i = 0; i < maxNumOfIngredients; i++){
+    chosenIngredients[i] = false;
+  }
+}
+
 void addSpacingY(int spacingY){
   textPosY += spacingY;
   startOfChoicesY = textPosY;
@@ -371,6 +537,22 @@ void printChoiceToPages(String message){
   printChoice(message);
   numOfChoicesOnPage[currentPage]++;
   numOfChoices = currentPage == 0 ? numOfChoicesOnPage[currentPage] : numOfChoicesOnPage[currentPage] + 1;  // one more choice: additional prev. Page Option
+}
+
+void printTanks(){
+  // print lines
+  mylcd.Set_Draw_color(WHITE);
+  mylcd.Draw_Fast_HLine(separatorLineX, separatorLineY, 400);
+  mylcd.Draw_Fast_VLine(separatorLineX, separatorLineY, 300);
+
+  int textPosXTanks = textPaddingX + separatorLineX;
+  int textPosYTanks = textPaddingY + separatorLineY;
+  mylcd.Print_String("Tanks:",textPosXTanks , textPosYTanks);
+  for(int i = 0; i < numOfTanks; i++){
+    textPosYTanks += textSpacingY;
+    String tankContent = cs.getTankContent(i) == "" ? "leer" : cs.getTankContent(i);
+    mylcd.Print_String(tankContent, textPosXTanks , textPosYTanks);
+  }
 }
 
 void goToNextPage(){
@@ -457,9 +639,162 @@ void resetGlobalVariablesForPage(){
 }
 
 
+// SD Card
+bool initializeSDCard(){
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(53)) {
+    Serial.println("initialization failed!");
+    state = "error";
+    return false;
+  }
+  Serial.println("initialization done.");
+  return true;
+}
 
+// Format: name, ingredient1, amount1, ingredient2, amount2, ..., ingredientn, amountn,;
+void downloadCocktailsFromSD(){
+  File cocktailTxt = SD.open("cocktail.txt");
+  if(!cocktailTxt){
+    // if the file didn't open, print an error:
+    Serial.println("error opening cocktail.txt");
+    state = "error";
+    return;
+  }
+  
+  Serial.println("downloading Cocktails");
 
+  for(int i = 0; i < maxNumOfCocktails; i++){
+    // name of Cocktail
+    String name = "";
+    while (cocktailTxt.available()) {
+      char currentChar = cocktailTxt.read();
+      if(currentChar == ',') break;
+      if(currentChar != '\n')
+        name += currentChar;
+    }
+    // get Ingredients
+    Ingredient ingredients[numOfTanks];
+    for(int j = 0; j < numOfTanks; j++){
+      if(cocktailTxt.read() == ';') break;
+      String nameOfIngredient = "";
+      while (cocktailTxt.available()) {
+        char currentChar = cocktailTxt.read();
+        if(currentChar == ',') break;
+        nameOfIngredient += currentChar;
+      }
 
+      // get rid of' ' and check for ';'
+      if(cocktailTxt.read() == ';') break;
+      String amountStr = "";
+      while (cocktailTxt.available()) {
+        char currentChar = cocktailTxt.read();
+        if(currentChar == ',') break;
+        amountStr += currentChar;
+      }
+
+      // Save Ingredient
+      ingredients[j] = Ingredient(nameOfIngredient, amountStr.toInt());
+      if(j != numOfTanks - 1)
+        ingredients[j + 1] = Ingredient();
+    }
+
+    // Save Cocktail
+    cocktails[i] = Cocktail(name, ingredients);
+    if(i != maxNumOfCocktails - 1)
+      cocktails[i + 1] = Cocktail();
+  }
+
+  // close File
+  cocktailTxt.close();
+
+/*
+  ingredients[0] = Ingredient("Tequila", 4);
+  ingredients[1] = Ingredient("Orangenlikoer", 4);
+  ingredients[2] = Ingredient();
+  cocktails[1] = Cocktail("Margheritha", ingredients);
+  cocktails[2] = Cocktail();
+  */
+}
+
+void downloadIngredientsFromSD(){
+  File ingredientsTxt = SD.open("ingred.txt");
+  if(!ingredientsTxt){
+    // if the file didn't open, print an error:
+    Serial.println("error opening ingred.txt");
+    state = "error";
+    return;
+  }
+  
+  Serial.println("downloading Ingredients");
+
+  for(int i = 0; i < maxNumOfIngredients; i++){
+    String name = "";
+    while (ingredientsTxt.available()) {
+      char currentChar = ingredientsTxt.read();
+      if(currentChar == ',') break;
+      if(currentChar != '\n')
+        name += currentChar;
+    }
+
+    // Save Ingredients
+    ingredients[i] = name;
+    if(i != maxNumOfIngredients - 1)
+      ingredients[i + 1] = "";
+  }
+
+  
+}
+
+void saveIngredientsFromCocktailsToSD(){
+  File ingredientsTxt = SD.open("ingred.txt");
+  if(!ingredientsTxt){
+    // if the file didn't open, print an error:
+    Serial.println("error opening ingred.txt");
+    state = "error";
+    return;
+  }
+
+  Serial.println("saving Ingredients");
+
+  for(int i = 0; i < maxNumOfCocktails; i++){
+    if(cocktails[i].getName() == "") return;
+
+    for(int j = 0; j < numOfTanks; j++){
+      // open file for reading
+      ingredientsTxt = SD.open("ingred.txt");
+
+      String ingredient = cocktails[i].getIngredient(j).getName();
+      if(ingredient == "") break;
+
+      // check if ingredient is in ingred.txt
+      bool isInside = false;
+      while(ingredientsTxt.available()){
+        String name = "";
+        while (ingredientsTxt.available()) {
+          char currentChar = ingredientsTxt.read();
+          if(currentChar == ',') break;
+          if(currentChar != '\n')
+            name += currentChar;
+        }
+        if(name == ingredient){
+          isInside = true;
+          break;
+        }
+      }
+
+      // if not write it to that file
+      if(!isInside){
+        ingredientsTxt.close();
+        ingredientsTxt = SD.open("ingred.txt", FILE_WRITE);
+        ingredientsTxt.print(ingredient);
+        ingredientsTxt.print(",");
+        ingredientsTxt.close();
+      }
+    }
+  }
+
+  ingredientsTxt.close();
+}
 
 
 
