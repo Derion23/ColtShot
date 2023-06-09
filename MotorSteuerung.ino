6fb27cffa56dae55cd6b539d02abbe2a14665676
RyyyanO <131525186+RyyyanO@users.noreply.github.com>
2023-06-10 00:12:59 +0200

    Update MotorSteuerung.ino

--- MotorSteuerung.ino
+++ MotorSteuerung.ino
@@ -1,7 +1,7 @@
 
 //Parameter fuer HauptMotor
 #define HauptMotorGeschwindigkeit 128 //0-255 (PWM)
-#define delayTime1 500 
+#define HauptMotorDelayTime 500 
 #define sensorPin A5
 #define ENA 9
 #define IN1 8
@@ -10,21 +10,21 @@
 //Parameter fuer NemaMotor(Ruehrer)
 #define dirPin1 26 
 #define stepPin1 27
-#define NemaMotor1Geschwindigkeit 2000  //je kleiner, desto schneller
+#define RuehrerNemaMotorGeschwindigkeit 2000  //je kleiner, desto schneller
   //Diese Zahl bedeutet, die Zeit(microseconds) zwischen jedem Schritt
-#define NemaMotor1Drehwinkel 400 //720° drehen
+#define RuehrerNemaMotorDrehwinkel 400 //720° drehen
    //fuer Stepper Motor: Jeder Step wird der Motor 1,8° drehen
-#define Verzoegerung1 500
+#define RuehrerVerzoegerung 500
 
 //Parameter fuer NemaMotor(Ausloeser)
 #define dirPin2 28
 #define stepPin2 29
-#define NemaMotor2Geschwindigkeit 2000
-#define NemaMotor2Drehwinkel 400
-#define Verzoegerung2 5000
+#define DosiererNemaMotorGeschwindigkeit 2000
+#define DosiererNemaMotorDrehwinkel 400
+#define DosiererVerzoegerung 5000
 
 //Parameter fuer DCMotor (Ruehrer)
-#define dcGeschwindigkeit 128
+#define RuehrerDC_Geschwindigkeit 128
 #define ENB 6
 #define IN3 5
 #define IN4 4
@@ -81,7 +81,7 @@ class StepperMotor{
 };
 
 DCMotor HauptMotor(ENA, IN1, IN2);
-DCMotor ruehrerDCMotor(ENB,IN3,IN4);
+DCMotor RuehrerDCMotor(ENB,IN3,IN4);
 StepperMotor RuehrerMotor(dirPin1, stepPin1);
 StepperMotor DosiererMotor(dirPin2, stepPin2);
 
@@ -100,7 +100,7 @@ void RotateClockwise(){
   bool Zustand = digitalRead(sensorPin);
   // HIGH, wenn nichts im Weg. LOW, wenn eine weisse Markierung auf der Flasche ist.
   HauptMotor.runClockwise();
-  delay(delayTime1); 
+  delay(HauptMotorDelayTime); 
   while(Zustand == 1){
     HauptMotor.runClockwise();
   }
@@ -111,7 +111,7 @@ void RotateCounterclockwise(){
   HauptMotor.setSpeed(HauptMotorGeschwindigkeit);
   bool Zustand = digitalRead(sensorPin);
   HauptMotor.runCounterClockwise();
-  delay(delayTime1);
+  delay(HauptMotorDelayTime);
   while(Zustand == 1){
     HauptMotor.runCounterClockwise();
   }
@@ -121,28 +121,28 @@ void RotateCounterclockwise(){
 //Funktion fuer Ruehrer
 
 void Ruehrer(){
-  ruehrerDCMotor.setSpeed(dcGeschwindigkeit);
+  RuehrerDCMotor.setSpeed(RuehrerDC_Geschwindigkeit);
 
   RuehrerMotor.setDirection(0);
-  RuehrerMotor.run(NemaMotor1Drehwinkel, NemaMotor1Geschwindigkeit);
-  delay(Verzoegerung1);
+  RuehrerMotor.run(RuehrerNemaMotorDrehwinkel, RuehrerNemaMotorGeschwindigkeit);
+  delay(RuehrerVerzoegerung);
 
-  ruehrerDCMotor.runClockwise();
+  RuehrerDCMotor.runClockwise();
   delay(runTime);
-  ruehrerDCMotor.stop();
-  delay(Verzoegerung1);
+  RuehrerDCMotor.stop();
+  delay(RuehrerVerzoegerung);
   
   RuehrerMotor.setDirection(1);
-  RuehrerMotor.run(NemaMotor1Drehwinkel,NemaMotor1Geschwindigkeit);
+  RuehrerMotor.run(RuehrerNemaMotorDrehwinkel,RuehrerNemaMotorGeschwindigkeit);
 }
 
 //Funktion fuer Dosierer
 void Dosierer(){
   DosiererMotor.setDirection(0);
-  DosiererMotor.run(NemaMotor2Drehwinkel,NemaMotor2Geschwindigkeit);
+  DosiererMotor.run(DosiererNemaMotorDrehwinkel,DosiererNemaMotorGeschwindigkeit);
 
-  delay(Verzoegerung2);
+  delay(DosiererVerzoegerung);
 
   DosiererMotor.setDirection(1);
-  DosiererMotor.run(NemaMotor2Drehwinkel,NemaMotor2Geschwindigkeit);
+  DosiererMotor.run(DosiererNemaMotorDrehwinkel,DosiererNemaMotorGeschwindigkeit);
 }
